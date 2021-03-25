0cb6c58059e8d35bec25740e840cde17472dc090
Sophie Bushchak <sophie.bushchak@hva.nl>
2021-03-25 17:07:07 +0100

    HTML structure, CSS reset, calculator operation functions

---
+++ script/calculator.js
@@ -0,0 +1,18 @@
+
+let currentNumber = 0;
+
+const multiply = (number1, number2) => {
+    return number1 * number2;
+}
+
+const add = (number1, number2) => {
+    return number1 + number2;
+}
+
+const substract = (number1, number2) => {
+    return number1 + number2;
+}
+
+const divide = (number1, number2) => {
+    return number1 / number2;
+}
