41fc6291d76f79e6842eac4cf8ae86377bf036da
Sophie Bushchak <sophie.bushchak@hva.nl>
2021-03-25 22:54:33 +0100

    Functional but with a few bugs
    Bugs:
    1. After calculating, if you type more it replaces the number and then does a strange string concatenation and uses the number you had previously again? Either way there is something weird with it.
    2. If you just press divide or multiply and then press the = button it gives NaN for some reason.

--- script/calculator.js
+++ script/calculator.js
@@ -1,5 +1,88 @@
-
+let inputMode = true;
 let currentNumber = 0;
+let operatorNumber = 0;
+let operator = null;
+
+let numberDisplay = document.querySelector(".number-display");
+let numberButtons = document.querySelectorAll(".btn-number");
+let operatorButtons = document.querySelectorAll('.btn-operator');
+let resultButton = document.querySelector('.btn-result');
+let clearButton = document.querySelector('.btn-clear');
+
+clearButton.addEventListener('click', () => {
+    clear();
+})
+
+for (let i = 0; i < numberButtons.length; i++) {
+   numberButtons[i].addEventListener('click', () => {
+       if (inputMode === true) {
+           if (currentNumber !== 0) {
+               currentNumber += numberButtons[i].innerText;
+           } else {
+               currentNumber = numberButtons[i].innerText;
+           }
+           numberDisplay.innerText = currentNumber;
+       } else {
+           if (operatorNumber !== 0) {
+               operatorNumber += numberButtons[i].innerText;
+           } else {
+               operatorNumber = numberButtons[i].innerText;
+           }
+           numberDisplay.innerText = operatorNumber;
+       }
+   });
+}
+
+for (let i = 0; i < operatorButtons.length; i++) {
+    operatorButtons[i].addEventListener('click', () => {
+        operator = operatorButtons[i].innerText;
+        inputMode = false;
+        numberDisplay.innerText = operatorNumber;
+    })
+}
+
+resultButton.addEventListener('click', () => {
+    if (operator !== null) {
+        calculate(operator);
+    }
+})
+
+const getOperation = (operator) => {
+    let operationFunction;
+    switch (operator) {
+        case '÷': {
+            operationFunction = divide;
+            break;
+        }
+        case '×': {
+            operationFunction = multiply;
+            break;
+        }
+        case '-': {
+            operationFunction = substract;
+            break;
+        }
+        case '+': {
+            operationFunction = add;
+        }
+    }
+    return operationFunction;
+}
+
+const clear = () => {
+    currentNumber = 0;
+    operatorNumber = 0;
+    inputMode = true;
+    numberDisplay.innerText = currentNumber;
+}
+
+const calculate = (operator) => {
+    const operation = getOperation(operator);
+    currentNumber = operation(parseInt(currentNumber), parseInt(operatorNumber));
+    numberDisplay.innerText = currentNumber;
+    operator = null;
+    operatorNumber = 0;
+}
 
 const multiply = (number1, number2) => {
     return number1 * number2;
@@ -10,9 +93,10 @@ const add = (number1, number2) => {
 }
 
 const substract = (number1, number2) => {
-    return number1 + number2;
+    return number1 - number2;
 }
 
 const divide = (number1, number2) => {
     return number1 / number2;
 }
+
