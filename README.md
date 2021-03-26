76ef82a5c0781ad41c32fb4f914648cdfc830098
Sophie Bushchak <sophie.bushchak@hva.nl>
2021-03-26 15:51:41 +0100

    Fix bugs and add backspace

---
+++ README.md
@@ -0,0 +1,21 @@
+# Simple JS Calculator v1
+## Introduction
+This calculator was created as part of an assignment in the Frontend Masters "Intro to Web Development v2" course. The challenge can be found here: https://btholt.github.io/intro-to-web-dev-v2/js-project/
+
+## Experience
+Prior to creating this calculator, I had not really worked with vanilla JS believe it or not, despite having worked with Angular on a team-based project and having had practice with React.
+
+My HTML and CSS are also not perfect, so it was a bit of a challenge for me to get the calculator in place. I ended up using several flexboxes as the rows for the buttons.
+
+For the JavaScript it was not too hard to figure out how to get the UI connected to the logic. Having worked with Angular before the concepts were already in my head. I did have to look up the syntax a bit.
+
+It was difficult to think of the structure of the program. I ended up managing state with some variables and connecting the buttons to functionality that was dependent on said variables. What I'm most proud of is that the calculations are done based on the operator that was selected, upon which an operation function is returned and used on the two present numbers.
+
+## Incomplete Features / Bad Code
+I'm pretty sure I did not manage to iron out all the bugs. I did catch a few of them and try to get them out as much as possible, but I think the way I managed the state prevented me from properly catching all of them.
+
+What I'm kind of unhappy with is the way I managed the state. I feel like I could have probably managed it in a better way where less variables are constantly updated in order to get the state. 
+
+There is also some code duplication because of the way I kept track of the numbers. I think I could fix this problem by refactoring the number state into a stack or an array of two numbers. Some of the checks and updates could have been changed into reusable functions probably.
+
+Once I learn more I would like the rewrite the CSS to be cleaner and refactor the logic.
--- script/calculator.js
+++ script/calculator.js
@@ -1,36 +1,64 @@
 let inputMode = true;
-let currentNumber = 0;
-let operatorNumber = 0;
+let firstInput = true;
+let currentNumber = '0';
+let operatorNumber = '0';
 let operator = null;
 
-let numberDisplay = document.querySelector(".number-display");
-let numberButtons = document.querySelectorAll(".btn-number");
+let numberDisplay = document.querySelector('.number-display');
+let numberButtons = document.querySelectorAll('.btn-number');
 let operatorButtons = document.querySelectorAll('.btn-operator');
 let resultButton = document.querySelector('.btn-result');
 let clearButton = document.querySelector('.btn-clear');
+let backspacebutton = document.querySelector('.btn-back');
 
 clearButton.addEventListener('click', () => {
     clear();
-})
+});
+
+backspacebutton.addEventListener('click', () => {
+    if (inputMode) {
+        currentNumber = backspaceNumber(currentNumber);
+        if (currentNumber === '') {
+            currentNumber = '0';
+        }
+        numberDisplay.innerText = currentNumber;
+    } else {
+        operatorNumber = backspaceNumber(operatorNumber);
+        if (operatorNumber === '0') {
+            operatorNumber = '0';
+        }
+        numberDisplay.innerText = operatorNumber;
+    }
+    debugState();
+});
+
 
 for (let i = 0; i < numberButtons.length; i++) {
-   numberButtons[i].addEventListener('click', () => {
-       if (inputMode === true) {
-           if (currentNumber !== 0) {
-               currentNumber += numberButtons[i].innerText;
-           } else {
-               currentNumber = numberButtons[i].innerText;
-           }
-           numberDisplay.innerText = currentNumber;
-       } else {
-           if (operatorNumber !== 0) {
-               operatorNumber += numberButtons[i].innerText;
-           } else {
-               operatorNumber = numberButtons[i].innerText;
-           }
-           numberDisplay.innerText = operatorNumber;
-       }
-   });
+    numberButtons[i].addEventListener('click', () => {
+        if (inputMode === true) {
+            if (firstInput === false) {
+                clear();
+            }
+            if (currentNumber !== '0') {
+                currentNumber += numberButtons[i].innerText;
+            } else {
+                currentNumber = numberButtons[i].innerText;
+                if (firstInput === false) {
+                    firstInput = true;
+                }
+            }
+            numberDisplay.innerText = currentNumber;
+        } else {
+            if (operatorNumber !== '0') {
+                operatorNumber += numberButtons[i].innerText;
+            } else {
+                operatorNumber = numberButtons[i].innerText;
+            }
+            numberDisplay.innerText = operatorNumber;
+        }
+        debugState();
+
+    });
 }
 
 for (let i = 0; i < operatorButtons.length; i++) {
@@ -38,14 +66,15 @@ for (let i = 0; i < operatorButtons.length; i++) {
         operator = operatorButtons[i].innerText;
         inputMode = false;
         numberDisplay.innerText = operatorNumber;
-    })
+        debugState();
+    });
 }
 
 resultButton.addEventListener('click', () => {
     if (operator !== null) {
         calculate(operator);
     }
-})
+});
 
 const getOperation = (operator) => {
     let operationFunction;
@@ -67,36 +96,54 @@ const getOperation = (operator) => {
         }
     }
     return operationFunction;
-}
+};
+
+const backspaceNumber = (numberDisplay) => {
+    return numberDisplay.slice(0, -1);
+};
 
 const clear = () => {
-    currentNumber = 0;
-    operatorNumber = 0;
+    currentNumber = '0';
+    operatorNumber = '0';
     inputMode = true;
+    firstInput = true;
     numberDisplay.innerText = currentNumber;
-}
+};
 
-const calculate = (operator) => {
-    const operation = getOperation(operator);
-    currentNumber = operation(parseInt(currentNumber), parseInt(operatorNumber));
+const calculate = (operatorSign) => {
+    const operation = getOperation(operatorSign);
+    currentNumber = operation(parseInt(currentNumber), parseInt(operatorNumber)).toString();
     numberDisplay.innerText = currentNumber;
     operator = null;
-    operatorNumber = 0;
-}
+    operatorNumber = '0';
+    inputMode = true;
+    firstInput = false;
+    debugState();
+};
 
 const multiply = (number1, number2) => {
     return number1 * number2;
-}
+};
 
 const add = (number1, number2) => {
     return number1 + number2;
-}
+};
 
 const substract = (number1, number2) => {
     return number1 - number2;
-}
+};
 
 const divide = (number1, number2) => {
     return number1 / number2;
-}
+};
+
+const debugState = () => {
+    console.log({
+        inputMode: inputMode,
+        firstInput: firstInput,
+        currentNumber: currentNumber,
+        operatorNumber: operatorNumber,
+        operator: operator,
+    });
+};
 
